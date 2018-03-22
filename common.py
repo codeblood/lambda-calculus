3aec252ac6e380baf4bff6498a03e9bdb0fe00f1
Cesar Barata <cesar.adbarata@gmail.com>
2018-03-22 15:39:30 -0300

    Added function composition and parameter flipping

--- common.py
+++ common.py
@@ -1,2 +1,8 @@
 # identity function
-ID    = lambda x: x
+ID = lambda x: x
+
+# function composition
+COMPOSE = lambda x: lambda f: lambda g: g(f(x))
+
+# flips a binary function's parameters
+FLIP = lambda f: lambda x: lambda y: f(y)(x)
